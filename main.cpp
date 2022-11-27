6e0fa5111b49fa835771fc0fc5a9a1a81646f299
yasur <yasur@LAPTOP-GK9UFHLD>
2022-11-27 15:40:22 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+using ll = long long;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	ll n;
+	cin >> n;
+
+	cout << n * n << '\n'
+		<< 2;
+
+	return 0;
+}
